8c6deb895289b054682ff6f60b0d6347d3c011b5
Andrew Kane <andrew@ankane.org>
2022-03-17 15:49:16 -0700

    Updated OpenSearch to 1.3.0

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require('path');
 const process = require('process');
 
 const versionMap = {
-  '1': '1.2.4',
+  '1': '1.3.0',
+  '1.3': '1.3.0',
   '1.2': '1.2.4',
   '1.1': '1.1.0',
   '1.0': '1.0.1'
