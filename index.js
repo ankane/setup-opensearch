70f915e78af59cd9dbf9f9a61759e42c85f31fa8
Andrew Kane <andrew@ankane.org>
2022-08-20 19:21:36 -0700

    Updated OpenSearch versions [skip ci]

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const path = require('path');
 const process = require('process');
 
 const versionMap = {
-  '2': '2.1.0',
+  '2': '2.2.0',
   '1': '1.3.4',
+  '2.2': '2.2.0',
   '2.1': '2.1.0',
   '2.0': '2.0.1',
   '1.3': '1.3.4',
