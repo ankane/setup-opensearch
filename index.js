576382186ac348942eb8285cae15a802f5102e38
Andrew Kane <andrew@ankane.org>
2025-04-16 13:29:38 -0700

    Added comment [skip ci]

--- index.js
+++ index.js
@@ -150,6 +150,7 @@ function installPlugins() {
     plugins = plugins.split(/\s*[,\n]\s*/);
 
     // validate
+    // do not change without checking impact on runBat
     plugins.forEach( function(plugin) {
       if (!/^\w(\w|-)+$/.test(plugin)) {
         throw `Invalid plugin: ${plugin}`;
